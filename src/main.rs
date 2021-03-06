74049c7e8ed9a48bf6e95c46cf516507915493d8
Lucas Werkmeister <mail@lucaswerkmeister.de>
2021-03-06 13:52:36 +0100

    First working version
    Tries to parse each line, prints the first encountered error and the bad
    line.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use serde_json::{self, Value};
+use std::io::{self, prelude::*};
+use std::process::exit;
+
+fn main() -> io::Result<()> {
+    for line in io::stdin().lock().lines() {
+        let line = line?;
+        if let Err(e) = serde_json::from_str::<Value>(&line) {
+            eprintln!("cjl: bad line: {}", e);
+            eprintln!("cjl: the bad line is:");
+            eprintln!("{}", line);
+            exit(1);
+        }
+    }
+    Ok(())
 }
