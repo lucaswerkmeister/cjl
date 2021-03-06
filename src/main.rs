c44d9f0d362b81ae5decc30e2e96362eb6a34b8d
Lucas Werkmeister <mail@lucaswerkmeister.de>
2021-03-06 14:35:39 +0100

    Print next line if available
    The next line could also be useful context, e.g. if the bad line was
    terminated by a spurious newline and the next line is actually supposed
    to be part of it.
    
    This requires us to manage the input a bit more manually, but it’s not
    too bad.

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,29 @@ use serde_json::{self, Value};
 use std::io::{self, prelude::*};
 use std::process::exit;
 
+fn strip_crlf(mut line: String) -> String {
+    if line.ends_with('\n') {
+        line.pop();
+        if line.ends_with('\r') {
+            line.pop();
+        }
+    }
+    line
+}
+
 fn main() -> io::Result<()> {
+    let stdin = io::stdin();
+    let mut input = stdin.lock();
     let mut previous_lines = GenericRingBuffer::<_, typenum::U4>::new();
-    for line in io::stdin().lock().lines() {
-        let line = line?;
+    loop {
+        let mut line = String::new();
+        let read_result = input.read_line(&mut line);
+        match read_result {
+            Ok(0) => return Ok(()),
+            Ok(_) => (),
+            Err(e) => return Err(e),
+        }
+        line = strip_crlf(line);
         if let Err(e) = serde_json::from_str::<Value>(&line) {
             eprintln!("cjl: bad line: {}", e);
             eprintln!("cjl: the previous (good) lines were:");
@@ -15,9 +34,15 @@ fn main() -> io::Result<()> {
             }
             eprintln!("cjl: the bad line is:");
             eprintln!("{}", line);
+            let mut next_line = String::new();
+            if let Ok(n) = input.read_line(&mut next_line) {
+                if n > 0 {
+                    eprintln!("cjl: the next line is:");
+                    eprintln!("{}", strip_crlf(next_line));
+                }
+            }
             exit(1);
         }
         previous_lines.push(line);
     }
-    Ok(())
 }
